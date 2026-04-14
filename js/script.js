fd0346680a509afea82ff720847a121d359f8dc1
FatFedCat <baev.fedar@yandex.ru>
2026-04-14 20:57:44 +0500

    балуюсь со скрипт  js

--- js/script.js
+++ js/script.js
@@ -45,16 +45,75 @@ confirm - всплывающее окно в верху, как alert тольк
 //     answer = confirm(`Ваш ответ ${result} \nХотите продолжить?`)
 // }
 //---------------------------------------
+const makeKeyedObject = (baseKey, count) => {
+	const obj = {};
+	for (let i = 0; i < count; i++) {
+		obj[baseKey + i] = i;
+	}
+	return obj;
+}
 
-let key = 'key'
-let obj = {
-	[key + '1']: 1,
-	[key + '2']: 2,
-	[key + '3']: 3
-};
-console.log(obj['key2'])
+const onClickByid = (id, handler) => {
+	const el = Byid(id); //! если добавить ! перед el то мы получим boolean значение
+	if (!el) return false;
+	el.addEventListener('click', handler);
+	return true;
+}
 
-let word = 'abcd'
-let b = [1,2,3]
-console.log('a' in word)
-console.log(1 in b)
+const hasIndex = (arr, index) => {
+	return index in arr;
+}
+
+const Byid = (id) => {
+	return document.getElementById(id);
+}
+
+const calcTable = document.querySelector('.calc-table');
+
+let expr = '';
+
+if (calcTable) {
+	calcTable.addEventListener('click', (e) => {
+		const btn = e.target.closest('button');
+		if (!btn) return;
+
+		const action = btn.dataset.action;
+		const val = btn.dataset.value;
+
+		if (action) {
+			switch (action) {
+				case 'clear':
+					expr = '';
+					break;
+				case 'backspace':
+					expr = expr.slice(0, -1);
+					break;
+				default:
+					// другие action пока игнорируем
+					break;
+			}
+		} else if (val) {
+			// цифры/операторы/точка приходят сюда через data-value
+			expr += val;
+		}
+
+		console.log('expr:', expr);
+		console.log('clicked:', {
+			text: btn.textContent.trim(),
+			value: val,
+			action: action,
+		});
+
+	});
+}
+
+
+
+
+onClickByid('visit-banner', () => console.log('click'));
+
+const obj = makeKeyedObject('key', 3);
+console.log(obj);
+console.log(obj['key2']);
+
+console.log(hasIndex([1, 2, 3], 2));
